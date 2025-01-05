50dc4cce391d8a18feea48f48fd5175448131fc9
Rashad1233 <73072692+Rashad1233@users.noreply.github.com>
2025-01-05 23:53:03 +0400

    Update app.py

--- web_project/app.py
+++ web_project/app.py
@@ -21,6 +21,15 @@ login_manager.login_view = 'login'  # Redirects to login page if not authenticat
 
 
 # ---------- Models ----------
+class Config:
+    SECRET_KEY = "supersecretkey"
+    DEBUG = False
+    # ... any other config
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secure_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'  # SQLite Database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
