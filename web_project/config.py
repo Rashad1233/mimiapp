cbec182c636fe0f3c8e6f164f09c7e6e8cd21464
Rashad1233 <rasadusubov973@gmail.com>
2025-01-05 22:59:29 +0400

    Initial commit

--- web_project/config.py
+++ web_project/config.py
@@ -2,6 +2,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secure_secret_key'  # Replace with a strong secret key
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secure_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'  # SQLite Database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
